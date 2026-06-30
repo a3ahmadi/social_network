13aeb202b353e0bb6d7c59f947e0f2e1b69dfb92
a3ahmadi <a09942999@gmail.com>
2026-06-30 14:23:13 +0330

    feat: implement user profile model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile'
+        )
+    
+    email = models.EmailField(
+        null=True, blank=True
+        )
+    
+    avatar = models.ImageField(
+        upload_to='profile_avatar',
+        null=True,
+        blank=True
+        )
+    
+    biography = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True
+        )
+    
+    website = models.URLField(
+        null=True,
+        blank=True
+        )
+    
+    is_private = models.BooleanField(
+        default=False
+        )
+    
+    followers_count = models.PositiveBigIntegerField(
+        default=0
+        )
+    
+    following_count = models.PositiveBigIntegerField(
+        default=0
+        )
+    
+    posts_count = models.PositiveBigIntegerField(
+        default=0
+        )
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True
+        )
+    
+    updated_at = models.DateTimeField(
+        auto_now=True
+        )
+    
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        ordering = ('-created_at',)
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    
+        
+
+
+
